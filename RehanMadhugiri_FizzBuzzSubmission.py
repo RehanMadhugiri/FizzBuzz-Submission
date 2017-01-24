c671f13049e528d67da997ae9439c9f133265dc7
Rehan Madhugiri <rehan.madhugiri@gmail.com>
2017-01-23 22:11:47 -0500

    Add files via upload

---
+++ RehanMadhugiri_FizzBuzzSubmission.py
@@ -0,0 +1,10 @@
+#This script prints out all integers from 1 to 100,
+#except replacing any number that is divisible by 3
+#with "Fizz", any number that is divisible by 5 with
+#”Buzz", and any number that is divisible by both 5 and 3 with "FizzBuzz."
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 != 0: print("Fizz")
+    elif i % 3 != 0 and i % 5 == 0: print("Buzz")
+    elif i % 3 == 0 and i % 5 == 0: print("FizzBuzz")
+    else: print(i)
